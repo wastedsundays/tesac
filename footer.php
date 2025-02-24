0439ea57aca928c35804f55a58e8d9aa82577d75
Adam H <ad.hauck@gmail.com>
2025-02-24 00:22:05 -0800

    Update footer layout and styling; enhance copyright and author information

--- footer.php
+++ footer.php
@@ -13,17 +13,9 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tesac-curl' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'tesac-curl' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'tesac-curl' ), 'tesac-curl', '<a href="http://adamh.ca">Adam H</a>' );
-				?>
+			<p class="footer-links"><a href="https://marpolecurling.ca">Marpole Curling Club</a></p>
+			<p class="copyright">&copy; <?php echo date('Y'); ?> TESAC Curling.</p>
+			<p class="footer-author">by <a href="https://www.adamh.ca">Adam H</a></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
