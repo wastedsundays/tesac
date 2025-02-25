8748e7b74a9c026ba7421a3c407e436d785d0a2a
Adam H <ad.hauck@gmail.com>
2025-02-24 16:09:04 -0800

    Update footer copyright format and enhance styling for home page and navigation

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
 			<p class="footer-links"><a href="https://marpolecurling.ca">Marpole Curling Club</a></p>
-			<p class="copyright">&copy; <?php echo date('Y'); ?> TESAC Curling.</p>
+			<p class="copyright">&copy; <?php echo date('Y'); ?> TESAC Curling</p>
 			<p class="footer-author">by <a href="https://www.adamh.ca">Adam H</a></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
